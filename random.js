4334e58fe8ae2f233e67e821377d90d293ec15dd
Shashank Sharma <shashank.sharma98@gmail.com>
2017-12-29 18:50:06 +0530

    Program added

---
+++ random.js
@@ -0,0 +1,32 @@
+var http    = require('http');
+var server  = http.createServer();
+var port    = process.env.PORT || 9001;
+
+var admin = require("firebase-admin");
+var serviceAccount = 'JSON_FILE_FOR_AUTHENTICATION'					// Repalce this
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'DATABASE_URL',										// Replace this
+});
+
+server.listen(port);
+
+server.once('request', function(req, res){
+	var db = admin.database();
+	var ref = db.ref("/");
+	ref.once("value", function(snapshot) {
+		fData = snapshot.val()
+		console.log(fData);
+	}, function (errorObject) {
+			console.log("The read failed: " + errorObject.code);
+		});
+});
+
+server.on('listening', function(){
+  console.log('# Visit http://localhost:' + port);
+});
+
+server.on('error', function(err){
+  console.log(err);
+});
